141989d7361bc27395dcdc1b966067556c3a09b5
Maddix <starcraftd@gmail.com>
2015-10-14 15:54:06 -0400

    FFmpeg string is built.
    I think I'm going to need to use strings with the file paths.

--- main.py
+++ main.py
@@ -35,49 +35,48 @@ import src.json_parser as parser
 class Handle_images():
 
 	def __init__(self, config):
-		self.source = self.create_path(config.get("image-options.source"))
-		self.destination = self.create_path(config.get("image-options.destination"),
-											config.get("image-options.create-destination-if-not-exists"))
-		self.working_folder = os.path.join(self.destination, time.strftime(config.get("image-options.file-name-and-date"), time.localtime()))
-		self.image_folder = "images"
-		self.video_folder = "video"
-		self.create_folders()
-		self.copied_images = self.copy_images(config.get("image-options.copy-image-type"))
+		self.source = self.create_path(config.get("folder-options.source"))
+		self.destination = self.create_path(config.get("folder-options.destination"),
+											config.get("folder-options.create-destination-if-not-exists"))
+		self.working_folder = os.path.join(self.destination, time.strftime(config.get("folder-options.file-name-and-date"), time.localtime()))
+		self.image_folder = os.path.join(self.destination, self.working_folder, config.get("folder-options.image-folder-name"))
+		self.video_folder = os.path.join(self.destination, self.working_folder, config.get("folder-options.video-folder-name"))
+		self.create_folders(self.working_folder, self.image_folder, self.video_folder)
+		self.copied_images = self.copy_images(self.source, self.image_folder, config.get("options.image-type"))
 		self.renumber_images(self.copied_images,
-							 config.get("image-options.copy-image-type"),
-							 config.get("image-options.number-of-zeros"))
-		#self.delete_images_from_card()
+							 self.image_folder,
+							 config.get("options.image-type"),
+							 config.get("options.image-number-total-pad"))
+		#self.delete_images_from_card(self.copied_images, self.source)
 
-	def create_folders(self):
+	def create_folders(self, working_folder, image_folder, video_folder):
 		""" Create three folders. """
-		os.mkdir(self.working_folder)
-		os.mkdir(os.path.join(self.working_folder, self.image_folder))
-		os.mkdir(os.path.join(self.working_folder, self.video_folder))
+		os.mkdir(working_folder)
+		os.mkdir(os.path.join(working_folder, image_folder))
+		os.mkdir(os.path.join(working_folder, video_folder))
 
-	def copy_images(self, image_type):
+	def copy_images(self, source, image_folder, image_type):
 		""" Copy all files from source with a specific ending to destination. """
 		copied = []
-		for listed_file in os.listdir(self.source):
+		for listed_file in os.listdir(source):
 			if listed_file.lower().endswith(image_type):
-				shutil.copy(os.path.join(self.source, listed_file),
-							os.path.join(self.destination, self.working_folder, self.image_folder))
+				shutil.copy(os.path.join(source, listed_file), image_folder)
 				copied.append(listed_file)
 		return copied
 
-	def renumber_images(self, image_name_list, image_type, total_zeros):
+	def renumber_images(self, image_name_list, image_folder, image_type, total_zeros):
 		""" Renumber each image. """
 		image_name_list.sort()
 		for number in range(len(image_name_list)):
-			path = os.path.join(self.destination, self.working_folder, self.image_folder)
 			image_name = "{0}{1}{2}".format("0"*(total_zeros - len(str(number))), str(number), image_type)
-			os.rename(os.path.join(path, image_name_list[number]), os.path.join(path, image_name))
+			os.rename(os.path.join(image_folder, image_name_list[number]), os.path.join(image_folder, image_name))
 
-	def delete_images_from_card(self):
+	def delete_images_from_card(self, copied_images, source):
 		""" WARNING! Be very sure what you are doing! This can wipe files from your drive.
 			Clears the images from the SD card once they have been copied to the PC.
 		"""
-		for copied_image in self.copied_images:
-			os.remove(os.path.join(self.source, copied_image))
+		for copied_image in copied_images:
+			os.remove(os.path.join(source, copied_image))
 
 	def create_path(self, path, create=False):
 		path = os.path.join(*path)
@@ -100,6 +99,8 @@ class Main():
 				("folder-options.destination", str),
 				("folder-options.create-destination-if-not-exists", bool),
 				("folder-options.file-name-and-date", str),
+				("folder-options.image-folder-name", str),
+				("folder-options.video-folder-name", str),
 			("options", dict),
 				("options.image-number-total-pad", int),
 				("options.image-name", str),
@@ -117,8 +118,19 @@ class Main():
 		self.config = parser.Handle_json(self.config_name, self.config_path, self.required_from_config)
 		self.handle_images = Handle_images(self.config)
 
-	def build_ffmpeg_command(self, config):
-		pass
+		print(self.build_ffmpeg_command(self.config, self.handle_images))
+
+	def build_ffmpeg_command(self, config, handle_images):
+		""" Add file paths and format ffmpeg-command string. """
+		command = config.get("ffmpeg-command")
+		index_of_input = command.index("-i")
+		# Input image
+		if index_of_input is not -1:
+			input_with_path = os.path.join(handle_images.image_folder, command.pop(index_of_input+1))
+			command.insert(index_of_input+1, input_with_path)
+		# Output video
+		command.append(os.path.join(handle_images.video_folder, command.pop(-1)))
+		return " ".join(command).format(**config.get("options"))
 
 	def launch_ffmpeg_command(self, built_command):
 		pass
