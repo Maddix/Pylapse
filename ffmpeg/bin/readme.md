301c8fb397001c9d44d061411a52e29b6a200fd7
Maddix <starcraftd@gmail.com>
2015-10-14 15:22:04 -0400

    Close to using FFmpeg.

--- ffmpeg/bin/readme.md
+++ ffmpeg/bin/readme.md
@@ -1,1 +1,1 @@
-Place ffmpeg.exe in here. You can get it from <a href=https://ffmpeg.org/download.html>here.</a>
+Place ffmpeg.exe in here. You can get it <a href=https://ffmpeg.org/download.html>here.</a>
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #! usr/bin/python3
-# Maddix - Oct 2015
+# Maddix - Oct 2015 - Python 3.4
 
 """
 The MIT License (MIT)
@@ -25,26 +25,27 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import os
+import subprocess
 import shutil
 import time
 # time.strftime("%Y-%m-%d_%I.%M%p", time.localtime())
 import src.json_parser as parser
 
 
-class Copy_images():
+class Handle_images():
 
-	def __init__(self, source, destination, create_destination, file_name_and_date, copy_image_type):
-		self.source = self.create_path(source)
-		self.destination = self.create_path(destination, create_destination)
-		self.copy_image_type = copy_image_type
-		self.working_folder = os.path.join(self.destination, time.strftime(file_name_and_date, time.localtime()))
-
+	def __init__(self, config):
+		self.source = self.create_path(config.get("image-options.source"))
+		self.destination = self.create_path(config.get("image-options.destination"),
+											config.get("image-options.create-destination-if-not-exists"))
+		self.working_folder = os.path.join(self.destination, time.strftime(config.get("image-options.file-name-and-date"), time.localtime()))
 		self.image_folder = "images"
 		self.video_folder = "video"
-
 		self.create_folders()
-		self.copied_images = self.copy_images()
-		self.renumber_images(self.copied_images)
+		self.copied_images = self.copy_images(config.get("image-options.copy-image-type"))
+		self.renumber_images(self.copied_images,
+							 config.get("image-options.copy-image-type"),
+							 config.get("image-options.number-of-zeros"))
 		#self.delete_images_from_card()
 
 	def create_folders(self):
@@ -53,23 +54,23 @@ class Copy_images():
 		os.mkdir(os.path.join(self.working_folder, self.image_folder))
 		os.mkdir(os.path.join(self.working_folder, self.video_folder))
 
-	def copy_images(self):
+	def copy_images(self, image_type):
 		""" Copy all files from source with a specific ending to destination. """
 		copied = []
 		for listed_file in os.listdir(self.source):
-			if listed_file.lower().endswith(self.copy_image_type):
+			if listed_file.lower().endswith(image_type):
 				shutil.copy(os.path.join(self.source, listed_file),
 							os.path.join(self.destination, self.working_folder, self.image_folder))
-				print(listed_file)
 				copied.append(listed_file)
 		return copied
 
-	def renumber_images(self, image_name_list):
+	def renumber_images(self, image_name_list, image_type, total_zeros):
 		""" Renumber each image. """
 		image_name_list.sort()
 		for number in range(len(image_name_list)):
 			path = os.path.join(self.destination, self.working_folder, self.image_folder)
-			os.rename(os.path.join(path, image_name_list[number]), os.path.join(path, str(number) + self.copy_image_type))
+			image_name = "{0}{1}{2}".format("0"*(total_zeros - len(str(number))), str(number), image_type)
+			os.rename(os.path.join(path, image_name_list[number]), os.path.join(path, image_name))
 
 	def delete_images_from_card(self):
 		""" WARNING! Be very sure what you are doing! This can wipe files from your drive.
@@ -94,22 +95,33 @@ class Main():
 		self.config_name = "config.json"
 		self.config_path = "src"
 		self.required_from_config = [
-			("image-options", dict),
-				("image-options.source", list),
-				("image-options.destination", list),
-				("image-options.create-destination-if-not-exists", bool),
-				("image-options.file-name-and-date", str),
-				("image-options.copy-image-type", str),
-			("ffmpeg-options", dict),
-				("ffmpeg-options.frames-per-second", int)
+			("folder-options", dict),
+				("folder-options.source", str),
+				("folder-options.destination", str),
+				("folder-options.create-destination-if-not-exists", bool),
+				("folder-options.file-name-and-date", str),
+			("options", dict),
+				("options.image-number-total-pad", int),
+				("options.image-name", str),
+				("options.image-type", str),
+				("options.image-per-second", int),
+				("options.image-start-number", int),
+				("options.video-framerate", int),
+				("options.video-codec", str),
+				("options.video-height", int),
+				("options.video-width", int),
+				("options.video-output-name", str),
+				("options.video-container", str),
+			("ffmpeg-command", list)
 		]
 		self.config = parser.Handle_json(self.config_name, self.config_path, self.required_from_config)
-		self.copy_images = Copy_images(
-			self.config.get("image-options.source"),
-			self.config.get("image-options.destination"),
-			self.config.get("image-options.create-destination-if-not-exists"),
-			self.config.get("image-options.file-name-and-date"),
-			self.config.get("image-options.copy-image-type"))
+		self.handle_images = Handle_images(self.config)
+
+	def build_ffmpeg_command(self, config):
+		pass
+
+	def launch_ffmpeg_command(self, built_command):
+		pass
 
 if __name__ == "__main__":
 	Main()
